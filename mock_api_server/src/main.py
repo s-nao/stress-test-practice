ee2a370e031f949ce92795c46a110efa73e913a8
s-nao <s.nao.dev@gmail.com>
2023-02-26 21:40:09 +0900

    コミット漏れ

--- mock_api_server/src/main.py
+++ mock_api_server/src/main.py
@@ -1,21 +1,75 @@
-# coding:utf-8
+from fastapi import FastAPI, Request
+from src.routers import search, users, auth
 
-import os, sys # 一つ上の階層をrootをしたいので
-sys.path.append(os.getcwd())
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
 
-from bottle import Bottle, error
-
-import controllers
+from fastapi.openapi.utils import get_openapi
 
 
-@error(500)
-def error500(error):
-    return "Server Error"
+class Settings(BaseModel):
+    authjwt_secret_key: str = "secret"
 
-if __name__ == "__main__":
-    app: Bottle = Bottle()
 
-    app.mount('/api', controllers.users.app)
-    app.mount('/api', controllers.search.app)
+app = FastAPI()
 
-    app.run(host='0.0.0.0', port=8081, debug=True, reloader=True)
+app.include_router(search.router)
+app.include_router(users.router)
+app.include_router(auth.router)
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+
+    # Custom documentation fastapi-jwt-auth
+    headers = {
+        "name": "Authorization",
+        "in": "header",
+        "required": True,
+        "schema": {
+            "title": "Authorization",
+            "type": "string"
+        },
+    }
+
+    # Get routes from index 4 because before that fastapi define router for /openapi.json, /redoc, /docs, etc
+    # Get all router where operation_id is authorize
+    router_authorize = [route for route in app.routes[4:] if route.operation_id == "authorize"]
+
+    for route in router_authorize:
+        method = list(route.methods)[0].lower()
+        try:
+            # If the router has another parameter
+            openapi_schema["paths"][route.path][method]['parameters'].append(headers)
+        except Exception:
+            # If the router doesn't have a parameter
+            openapi_schema["paths"][route.path][method].update({"parameters":[headers]})
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
